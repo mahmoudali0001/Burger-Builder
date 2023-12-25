5c25528fbb9d20897b846e0abd67f1c499fafb85
Mahmoud Ali <mahmoudalifarag1@gmail.com>
2023-12-25 21:40:45 +0200

    fixed redux error verison

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
+import store from "./Store/createStore";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
 const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 const root = createRoot(document.getElementById("root"));
